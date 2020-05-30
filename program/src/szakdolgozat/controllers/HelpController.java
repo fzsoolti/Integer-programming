4a3846a53d501911e3f956ffd6c6b2c7474683e9
fzsoolti <43071361+fzsoolti@users.noreply.github.com>
2020-05-30 21:22:59 +0200

    Add files via upload

--- program/src/szakdolgozat/controllers/HelpController.java
+++ program/src/szakdolgozat/controllers/HelpController.java
@@ -25,19 +25,32 @@ public class HelpController {
     }
 
     public void helpBackpack(){
-        itemDetailsTextArea.setText("Hátizsák feladat: \n Hátizsák probléma megoldására.");
+        itemDetailsTextArea.setText("Hátizsák probléma megoldására. Az algoritmus csak akkor indítható, ha van már hozzáadott tárgy.\n " +
+                "Tárgy csak akkor adható hozzá, ha: \n " +
+                "  - a 'Tárgy neve' mező nem üres \n" +
+                "  - a tárgy súlya pozitív szám\n" +
+                "  - a tárgy hasznossága egy és tíz közé eső egész szám");
     }
 
     public void helpCoin(){
-        itemDetailsTextArea.setText("Pénzváltási probléma: \n Pénzváltási probléma megoldására.");
+        itemDetailsTextArea.setText("Pénzváltási probléma megoldására. Adott összeg kifizetése minimális címlettel (Forint) \n" +
+                "Az algoritmus csak helyes input megadása esetén indítható, a: \n" +
+                "  - mimimum: 3,\n" +
+                "  - maximum: 2 147 483 647,\n" +
+                "közé eső egész számok esetén.");
     }
 
     public void helpSet1(){
-        itemDetailsTextArea.setText("Halmazlefedési feladat: \n Halmazlefedési probléma megoldására.");
+        itemDetailsTextArea.setText("Halmazlefedési probléma megoldására. Az algoritmusok csak akkor indíthatóak, ha van már hozzáadott halmaz.\n" +
+                "Halmaz csak akkor adható hozzá, ha: \n" +
+                "  - a részhalmazok vesszővel elválasztva vannak megadva,\n" +
+                "  - ki van választva egy algoritmus");
     }
 
     public void helpSet2(){
-        itemDetailsTextArea.setText("Halmazfelbontási feladat: \n Halmazfelbontási probléma megoldására.");
+        itemDetailsTextArea.setText("Halmazfelbontási probléma megoldására. Az algoritmus csak akkor indítható, ha van már hozzáadott halmaz.\n" +
+                "Halmaz csak akkor adható hozzá, ha: \n" +
+                "  - a részhalmazok vesszővel elválasztva vannak megadva");
     }
 
 }
